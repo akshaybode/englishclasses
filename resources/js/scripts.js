5d2dc0a24f6973db0315f34824286ef07dffce9e
Akshay Bode <akshaybode087@gmail.com>
2019-04-11 19:46:07 +0530

    All working well

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -9,5 +9,51 @@ $(document).ready(function(){
 			$('nav').removeClass('sticky');
 		}
     });
+    
+    /*---Scoll on buttons----*/
+    $('.scroll-to-more').click(function() {
+      $('html,body').animate({scrollTop: $('.js--section-features').offset().top},1000);
+    });
+    
+    
+    /* ---Navigation scroll----*/
+    
+    $(function() {
+  		$('a[href*=#]:not([href=#])').click(function() {
+    		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      		var target = $(this.hash);
+      		target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+	      		if (target.length) {
+	        		$('html,body').animate({
+	         		 scrollTop: target.offset().top
+	        		}, 1000);
+	        		return false;
+	      		}
+   			}
+ 		});
+	});
+    
+    /* Add Animations on scroll */
+
+	$('.js--wp-1').waypoint(function(direction) {
+		$('.js--wp-1').addClass('animated fadeIn');
+	}, {
+		offset: '50%'
+	});
+    
+    $('.js--wp-2').waypoint(function(direction) {
+		$('.js--wp-2').addClass('animated pulse');
+	}, {
+		offset: '50%'
+	});
+    
+    $('.js--wp-3').waypoint(function(direction) {
+		$('.js--wp-1').addClass('animated fadeInUp');
+	}, {
+		offset: '50%'
+	});
+    
+    
+    
 });
 
